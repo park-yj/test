2760c28ec6a49a46c1b1d4ac407c2d58e0646235
park3 <park3@LAPTOP-TPOODNE6>
2019-01-04 16:18:14 +0900

    a

--- test/src/main/java/com/home/study/HomeController.java
+++ test/src/main/java/com/home/study/HomeController.java
@@ -1,4 +1,4 @@
-package com.home.controller;
+package com.home.study;
 
 import java.text.DateFormat;
 import java.util.Date;
--- test/src/main/java/com/home/study/LoginController.java
+++ test/src/main/java/com/home/study/LoginController.java
@@ -1,4 +1,4 @@
-package com.home.controller;
+package com.home.study;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
